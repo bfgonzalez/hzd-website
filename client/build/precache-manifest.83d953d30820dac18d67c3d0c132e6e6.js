35f3361c107a2e712e622397a38c2581adb086e1
bfgonzalez <bfgonzalez142@gmail.com>
2020-07-13 13:39:40 +0800

    connect react app to express server

---
+++ client/build/precache-manifest.83d953d30820dac18d67c3d0c132e6e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c57cebfb3665df3a6ba2aa70bcee787",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e3b47f0ab9e30dda6e71",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e50bfbcf903bbbe834d2",
+    "url": "/static/js/2.2b0b3bb6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2b0b3bb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3b47f0ab9e30dda6e71",
+    "url": "/static/js/main.ed363a6b.chunk.js"
+  },
+  {
+    "revision": "d31f996570ac5d8f029d",
+    "url": "/static/js/runtime-main.b82dcb2c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
