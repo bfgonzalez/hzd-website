8d08c3073e97497303616a96fa0fc3a3fdd3a7d5
bfgonzalez <bfgonzalez142@gmail.com>
2020-07-15 21:31:03 +0800

    display data from api in table

--- client/src/components/Machines/Machines.js
+++ client/src/components/Machines/Machines.js
@@ -1,30 +1,45 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
+const headers = ['Name', 'Size', 'Origin', 'Override', 'Class', 'Machine Sites', 'Weakness', 'Strength', 'Weak Points'];
+
 const Machines = () => {
   const [machines, setMachines] = useState([])
 
   useEffect(() => {
     axios.get(`${process.env.REACT_APP_API_URL}`)
-    .then(response => {
-      console.log(response.data[0])
-      setMachines(response.data)
-    })
-    .catch(error => {
-      console.log(error);
-    })
+    .then(response => setMachines(response.data))
+    .catch(error => console.log(error))
   }, [])
 
+
   return (
-    <div>
-      <ol>
+    <table className="table">
+      <thead>
+        <tr>
+          {headers.map((header, index) => (
+            <th key={index}>{header}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
         {machines.map((machine, index) => {
           return (
-          <li>{Object.values(machine)}</li>
+            <tr key={index}>
+              <td>{machine.name}</td>
+              <td>{machine.size}</td>
+              <td>{machine.origin}</td>
+              <td>{machine.override}</td>
+              <td>{machine.class}</td>
+              <td>{machine.machine_sites}</td>
+              <td>{machine.weakness}</td>
+              <td>{machine.strength}</td>
+              <td>{machine.weak_points}</td>
+            </tr>
           )
         })}
-      </ol>
-    </div>
+      </tbody>
+    </table>
   )
 }
 
