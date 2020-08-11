253bc98f6bf6e5ccf768426bf55375c149306e13
bfgonzalez <bfgonzalez142@gmail.com>
2020-08-11 19:29:32 +0800

    machines(feat): add filtering options
    add functionality for filtering machines based on query parameters

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,5 +5,6 @@ const api = require('../controllers/views');
 module.exports = (app) => {
   app.get('/api/machines', machinesController.list);
   app.get('/api/views/machines', api.list);
+  app.get('/api/machines/filter', machinesController.filterList);
   app.post('/api/machines', machinesController.create);
 }
