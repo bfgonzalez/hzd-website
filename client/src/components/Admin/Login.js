13aa3d24911f79b9b85f4c087345b2921445926e
bfgonzalez <bfgonzalez142@gmail.com>
2020-08-18 17:47:42 +0800

    app(style): fix styling
    fix content centering of login page, fix padding for machines page

--- client/src/components/Admin/Login.js
+++ client/src/components/Admin/Login.js
@@ -1,44 +1,64 @@
-import React, { useState } from "react";
-import { useHistory } from "react-router-dom";
+import React, { useState } from "react"
+import { useHistory } from "react-router-dom"
 
-import Layout from "../Template/Layout";
+import Layout from "../Template/Layout"
 
 const Login = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+	const [username, setUsername] = useState("")
+	const [password, setPassword] = useState("")
 
-  const history = useHistory();
+	const history = useHistory()
 
-  const handleSubmit = event => {
-    event.preventDefault()
-    if (username === `${process.env.REACT_APP_USERNAME}` && password === `${process.env.REACT_APP_PASSWORD}`) {
-      history.push(`${process.env.REACT_APP_ADMIN_URL}`)
-    } else {
-      alert("unauthorized user")
-    }
-  }
+	const handleSubmit = (event) => {
+		event.preventDefault()
+		if (
+			username === `${process.env.REACT_APP_USERNAME}` &&
+			password === `${process.env.REACT_APP_PASSWORD}`
+		) {
+			history.push(`${process.env.REACT_APP_ADMIN_URL}`)
+		} else {
+			alert("unauthorized user")
+		}
+	}
 
-
-  return (
-    <Layout>
-      <div className="login-section">
-        <h1 className="title has-text-white">Login</h1>
-        <form onSubmit={handleSubmit}>
-          <div className="field">
-            <div className="control">
-              <input className="input is-primary" name="username" type="text" placeholder="Username" onChange={event => setUsername(event.target.value)}/>
-            </div>
-          </div>
-          <div className="field">
-            <div className="control">
-              <input className="input is-primary" name="password" type="password" placeholder="Password" onChange={event => setPassword(event.target.value)}/>
-            </div>
-          </div>
-          <button className="button is-primary has-text-weight-bold" type="submit">Login</button>
-        </form>
-      </div>
-    </Layout>
-  )
+	return (
+		<Layout>
+			<div className="columns is-centered is-vcentered full-height">
+				<div className="login-section column is-4 is-10-mobile">
+					<h1 className="title has-text-white">Login</h1>
+					<form onSubmit={handleSubmit}>
+						<div className="field">
+							<div className="control">
+								<input
+									className="input is-primary"
+									name="username"
+									type="text"
+									placeholder="Username"
+									onChange={(event) => setUsername(event.target.value)}
+								/>
+							</div>
+						</div>
+						<div className="field">
+							<div className="control">
+								<input
+									className="input is-primary"
+									name="password"
+									type="password"
+									placeholder="Password"
+									onChange={(event) => setPassword(event.target.value)}
+								/>
+							</div>
+						</div>
+						<button
+							className="button is-primary has-text-weight-bold"
+							type="submit">
+							Login
+						</button>
+					</form>
+				</div>
+			</div>
+		</Layout>
+	)
 }
 
-export default Login;
+export default Login
--- client/src/components/Machines/Machines.js
+++ client/src/components/Machines/Machines.js
@@ -1,70 +1,103 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react"
+import axios from "axios"
 
-import Layout from '../Template/Layout';
-import Table from '../Template/Table';
-import ButtonLink from '../Template/ButtonLink';
+import Layout from "../Template/Layout"
+import Table from "../Template/Table"
+import ButtonLink from "../Template/ButtonLink"
 
-const machineHeaders = ['Name', 'Size', 'Origin', 'Override', 'Class', 'Machine Sites', 'Weakness', 'Strength', 'Weak Points', 'Explosive Components'];
+const machineHeaders = [
+	"Name",
+	"Size",
+	"Origin",
+	"Override",
+	"Class",
+	"Machine Sites",
+	"Weakness",
+	"Strength",
+	"Weak Points",
+	"Explosive Components",
+]
 
 const Machines = ({ isAdmin }) => {
-  const [machines, setMachines] = useState([])
-  const [searchTerm, setSearchTerm] = useState("");
-  const filteredMachines = [];
+	const [machines, setMachines] = useState([])
+	const [searchTerm, setSearchTerm] = useState("")
+	const filteredMachines = []
 
-  useEffect(() => {
-    axios.get(`${process.env.REACT_APP_API_URL}`)
-    .then(response => {
-      // on initial page load, or if search bar is empty, display full list of machines
-      if(searchTerm === "") {
-        setMachines(response.data)
-      }
-      // if search bar is not empty, filter machines based on input value
-      else {
-        machines.filter(machine => {
-          let search = searchTerm.toLowerCase();
-          let values = Object.values(machine); // get values of each machine object
+	useEffect(() => {
+		axios
+			.get(`${process.env.REACT_APP_API_URL}`)
+			.then((response) => {
+				// on initial page load, or if search bar is empty, display full list of machines
+				if (searchTerm === "") {
+					setMachines(response.data)
+				}
+				// if search bar is not empty, filter machines based on input value
+				else {
+					machines.filter((machine) => {
+						let search = searchTerm.toLowerCase()
+						let values = Object.values(machine) // get values of each machine object
 
-          for (let value of values) {
-            if(value !== null && value.toString().toLowerCase().includes(search)) {
-              // if any of the values include the search term, push the machine into the filteredMachines array
-              filteredMachines.push(machine);
-              break; // break out of the loop as soon as the condition is met the first time
-            }
-          }
-        });
-        setMachines(filteredMachines);
-      }
-    })
-    .catch(error => console.log(error))
-    // eslint-disable-next-line
-  }, [searchTerm])
+						for (let value of values) {
+							if (
+								value !== null &&
+								value.toString().toLowerCase().includes(search)
+							) {
+								// if any of the values include the search term, push the machine into the filteredMachines array
+								filteredMachines.push(machine)
+								break // break out of the loop as soon as the condition is met the first time
+							}
+						}
+					})
+					setMachines(filteredMachines)
+				}
+			})
+			.catch((error) => console.log(error))
+		// eslint-disable-next-line
+	}, [searchTerm])
 
-  const handleInputChange = event => {
-    setSearchTerm(event.target.value);
-  }
+	const handleInputChange = (event) => {
+		setSearchTerm(event.target.value)
+	}
 
-  return (
-    <Layout>
-      <div className="machines-section pt-6">
-        <h1 className="pt-6 has-text-white title is-inline">Machine Catalogue</h1>
-        {isAdmin &&
-            <div className="field is-grouped is-pulled-right">
-              <ButtonLink text="Add Machine" color="primary" link="/admin/add-machine"/>
-              <ButtonLink text="Edit Machine" color="warning" link="/admin/edit-machine"/>
-              <ButtonLink text="Delete Machine" color="danger" link="/admin/delete-machine"/>
-            </div>
-        }
-        <div className="field mt-4">
-          <div className="control">
-            <input className="input is-primary" name="search" type="text" onChange={handleInputChange} value={searchTerm} placeholder="Search Machines"/>
-          </div>
-        </div>
-        <Table headers={machineHeaders} data={machines}/>
-      </div>
-    </Layout>
-
-  )
+	return (
+		<Layout>
+			<div className="machines-section">
+				<h1 className="has-text-white title is-inline">Machine Catalogue</h1>
+				{isAdmin && (
+					<div className="field is-grouped is-pulled-right">
+						<ButtonLink
+							text="Add Machine"
+							color="primary"
+							link="/admin/add-machine"
+						/>
+						<ButtonLink
+							text="Edit Machine"
+							color="warning"
+							link="/admin/edit-machine"
+						/>
+						<ButtonLink
+							text="Delete Machine"
+							color="danger"
+							link="/admin/delete-machine"
+						/>
+					</div>
+				)}
+				<div className="field mt-4">
+					<div className="control">
+						<input
+							className="input is-primary"
+							name="search"
+							type="text"
+							onChange={handleInputChange}
+							value={searchTerm}
+							placeholder="Search Machines"
+						/>
+					</div>
+				</div>
+				<Table headers={machineHeaders} data={machines} />
+			</div>
+		</Layout>
+	)
 }
 
-export default Machines;
+export default Machines
