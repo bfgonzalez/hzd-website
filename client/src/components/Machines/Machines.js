ba77c871aaf6e6a8894c8f00d5824265d99d73be
bfgonzalez <bfgonzalez142@gmail.com>
2020-08-27 16:08:51 +0800

    machines(feat): add confirm dialogue before deleting a machine
    add confirm dialogue that triggers before deleting a machine, fix mobile responsiveness for machine details

--- client/src/components/Machines/Machines.js
+++ client/src/components/Machines/Machines.js
@@ -83,7 +83,7 @@ const Machines = ({ isAdmin }) => {
         ) : (
           <div className="columns is-multiline mt-2">
             {machines.map((machine, index) => (
-              <div className="column is-one-third">
+              <div className="column is-one-third" key={index}>
                 <MachineCard data={machine} index={index} isAdmin={isAdmin} />
               </div>
             ))}
