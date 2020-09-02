429baed08daf570d956973e35055fea4205ed164
bfgonzalez <bfgonzalez142@gmail.com>
2020-09-02 12:17:06 +0800

    app(feat): add metadata, install react-helmet

---
+++ client/src/components/SEO/SEO.js
@@ -0,0 +1,77 @@
+import React from "react"
+import { Helmet } from "react-helmet"
+import PropTypes from "prop-types"
+
+const SEO = () => {
+  const title = "Horizon Zero Dawn Wiki"
+  const description = "A wiki for Horizon Zero Dawn"
+  const url = "https://hzd-website.herokuapp.com/"
+
+  return (
+    <Helmet
+      htmlAttributes={{
+        lang,
+      }}
+      title="Horizon Zero Dawn Wiki"
+      meta={[
+        {
+          name: `description`,
+          content: description,
+        },
+        {
+          name: `url`,
+          content: url,
+        },
+        {
+          property: `og:title`,
+          content: title,
+        },
+        {
+          property: `og:description`,
+          content: description,
+        },
+        {
+          property: `og:image`,
+          content: `${url}/thumbnail.png`,
+        },
+        {
+          property: `og:type`,
+          content: `website`,
+        },
+        {
+          name: `twitter:card`,
+          content: `summary`,
+        },
+        {
+          name: `twitter:creator`,
+          content: `Bianca Francesca Gonzalez | @bfgonzalez`,
+        },
+        {
+          name: `twitter:title`,
+          content: title,
+        },
+        {
+          name: `twitter:description`,
+          content: description,
+        },
+      ]}
+    />
+  )
+}
+
+SEO.defaultProps = {
+  lang: `en`,
+  meta: [],
+  description: ``,
+}
+
+SEO.propTypes = {
+  description: PropTypes.string,
+  lang: PropTypes.string,
+  meta: PropTypes.arrayOf(PropTypes.object),
+  title: PropTypes.string,
+  thumbnail: PropTypes.string,
+  url: PropTypes.string,
+}
+
+export default SEO
